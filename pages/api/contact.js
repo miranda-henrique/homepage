121a7c61707154d258c9ab741a23437e2def6ec8
Henrique Miranda <miranda.henrique@outlook.com.br>
2022-05-12 17:32:00 -0300

    Upgrade to @sendgrid/mail

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,29 +1,15 @@
 /* eslint-disable import/no-anonymous-default-export */
-/* eslint-disable no-unused-vars */
-import nodemailer from 'nodemailer'
-import sgTransport from 'nodemailer-sendgrid-transport'
+const sgMail = require('@sendgrid/mail')
 
-export default (request, response) => {
+export default async (request, response) => {
   const {
     name: requestName,
     email: requestEmail,
     phone: requestPhone
   } = request.body
+  sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY)
 
-  console.log({ requestName })
-  console.log({ requestEmail })
-  console.log({ requestPhone })
-
-  let options = {
-    auth: {
-      api_user: process.env.NEXT_PUBLIC_SENDGRID_USERNAME,
-      api_key: process.env.NEXT_PUBLIC_SENDGRID_API_KEY
-    }
-  }
-
-  let client = nodemailer.createTransport(sgTransport(options))
-
-  let email = {
+  const msg = {
     from: process.env.NEXT_PUBLIC_SENDER,
     to: process.env.NEXT_PUBLIC_RECEIVER,
     subject: 'Novo contato de Homepage',
@@ -31,14 +17,11 @@ export default (request, response) => {
     html: `<b><h1>Novo contato de Homepage:</h1> <br /><h3><strong>Nome:</strong> ${requestName}</h3> <br /><h3><strong>Email:</strong> ${requestEmail}</h3> <br /><h3><strong>Telefone:</strong> ${requestPhone}</h3></b>`
   }
 
-  client.sendMail(email, function (err, info) {
-    if (err) {
-      console.error(err)
-    } else {
-      console.log({ info })
-      console.log('Message sent: ')
-    }
-  })
-
-  response.status(200).json(request.body)
+  try {
+    const result = await sgMail.send(msg)
+    console.log('Email sent', result)
+    response.status(200).json(result)
+  } catch (error) {
+    console.error(error)
+  }
 }
